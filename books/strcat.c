018da5a6eb7f54825be5ea3890100ecab03014b7
Eric(黃梃家) <102838561+eric2003-tj@users.noreply.github.com>
2022-07-30 11:40:54 +0800

    Create strcat.c

---
+++ books/strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+int main(void){
+  char sources[80];
+  char destinations[80];
+  scanf("%s%s",sources,destinations);
+  printf("%s\n",destinations);
+  strcat(destinations,sources);
+  printf("%s\n",destinations);
+  return 0;
+}
