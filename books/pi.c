806fcf2c11a3860685037d03b28939d4086c304a
Eric(黃梃家) <102838561+eric2003-tj@users.noreply.github.com>
2022-08-03 13:19:57 +0800

    Create pi.c

---
+++ books/pi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+int main(void)
+{
+   double radius;
+   const double pi = 3.1415926;
+   scanf("%lf",&radius);
+   printf("%lf\n",radius*2*pi);
+}
