40a5e9aefac19f3b0ff473cb713dfcbb605a4bcf
Eric(黃梃家) <102838561+eric2003-tj@users.noreply.github.com>
2022-08-04 09:17:22 +0800

    Create structure-init.c

---
+++ books/structure-init.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+struct student{
+  char name[20];
+  int id;
+  char phone_number[15];
+  float grade;
+  int birthyear,birthmonth,birthday;
+};
+
+int main(void)
+{ struct student john = {"John Smith",12345,"0983275112",4.0,1993,8,2};
+  printf("name is %s\n",john.name);
+  return 0;
+}
